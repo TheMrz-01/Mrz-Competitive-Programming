b361f046002a023dbacca6b7c31af66445326315
Mrz <86775732+TheMrz-01@users.noreply.github.com>
2024-10-27 18:09:00 +0300

    Put the search function and make some adjustments

--- Data-Algorithms-C/bts.c
+++ Data-Algorithms-C/bts.c
@@ -9,6 +9,10 @@ typedef struct Node{
 
 Node* createNode(int data){
     Node* newNode = (Node*)malloc(sizeof(Node));
+    if (newNode == NULL) {
+        printf("Memory allocation failed\n");
+        exit(1); 
+    }
     newNode->data = data;
     newNode->left = NULL;
     newNode->right = NULL;
@@ -19,6 +23,7 @@ Node* createNode(int data){
 void insertNode(Node** root,int data){
     if(*root == NULL){
         *root = createNode(data);
+        return;
     }
 
     if(data <= (*root)->data){
@@ -29,18 +34,48 @@ void insertNode(Node** root,int data){
     }
 }
 
+void searchData(Node** root,int data){
+    if(*root == NULL){
+        printf("The bst is empty!");
+        return;
+    }
+
+    if(data == (*root)->data){
+        printf("Yes sir\n");
+        return;
+    }
+    else if(data <= (*root)->data){
+        searchData(&((*root)->left), data);
+    }
+    else if(data > (*root)->data){
+        searchData(&((*root)->right), data);   
+    }
+    else{
+        printf("Where is my data bruce");
+        return;
+    }
+}
+
 int main(int argc,char *argv[]){
     //Nothing to look at here browski
     if(argc > 1){
         return 0;
     }
 
+    printf("Started\n");
+
     //In this example I will use a root that's not global and to keep main function clean I will pass the adress of the root
     struct Node* root = NULL;
 
-    insert(&root,15);
-    insert(&root,10);
-    insert(&root,20);
+    insertNode(&root,15);
+    insertNode(&root,10);
+    insertNode(&root,20);
+
+    searchData(&root,15);
+    searchData(&root,10);
+    searchData(&root,20);
+
+    searchData(&root,16);
 
     //-----------------------
     int closeWindow;
